dce5382d7334f3bd09df62a98719cb72920e80e6
jnims <jmnims@gmail.com>
2015-09-27 00:01:01 -0400

    Implemented code for intEntry class

---
+++ MP3/Source.cpp
@@ -0,0 +1,11 @@
+#include <list>
+#include "intEntry.h"
+
+using namespace std;
+
+void loadPrimes(list<intEntry>& primes, int n);
+
+void main()
+{
+
+}
--- MP3/intEntry.h
+++ MP3/intEntry.h
@@ -1,34 +1,64 @@
 #pragma once
 
 #include <iostream>
+
+
 using namespace std;
 
 class intEntry
 {
-public:
-    // initialize the integer value and its count
-    intEntry(int v, int c = 1);
-
-    // return value
-    int getValue() const;
-
-    // return count
-    int getCount() const;
-
-    // increment count
-    void increment();
-
-    // compare lhs and rhs using value
-    friend bool operator< (const intEntry& lhs, const intEntry& rhs);
-    friend bool operator== (const intEntry& lhs, const intEntry& rhs);
-
-    // output obj in format "value value ... value" (count times)
-    friend ostream& operator<< (ostream& ostr, const intEntry& obj);
-
 private:
     int value;
     // integer value
+
     int count;
     // number of occurrences of value
+
+public:
+    // initialize the integer value and its count
+    intEntry(int v, int c = 1)
+    {
+        value = v;
+        count = c;
+    }
+
+    // return value
+    int getValue() const
+    {
+        return value;
+    }
+
+    // return count
+    int getCount() const
+    {
+        return count;
+    }
+
+    // increment count
+    void increment()
+    {
+        count++;
+    }
+
+    // compare lhs and rhs using value
+    friend bool operator< (const intEntry& lhs, const intEntry& rhs)
+    {
+        return lhs.value < rhs.value;
+    }
+
+    friend bool operator== (const intEntry& lhs, const intEntry& rhs)
+    {
+        return lhs.value == rhs.value;
+    }
+
+    // output obj in format "value value ... value" (count times)
+    friend ostream& operator<< (ostream& ostr, const intEntry& obj);
 };
+
+ostream& operator<< (ostream& ostr, const intEntry& obj)
+{
+    for (int i = 0; i < obj.count; i++)
+        ostr << obj.value << " ";
+    return ostr;
+}
 
